708031ae820b5ecdd4db44b93cb673a9724051e4
sansanehi <sinusanehi@gmail.com>
2025-07-03 20:30:33 +0530

    new updates

--- frontend/src/config/supabase.js
+++ frontend/src/config/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dwukxszngzhfoldnodrx.supabase.co"; // <-- Replace with your Supabase project URL
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3dWt4c3puZ3poZm9sZG5vZHJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NDAzMDAsImV4cCI6MjA2NzExNjMwMH0.3IGzmi-WWAaL23OamXVu2GGuSGU-hPuv5kFeGT_KjGE"; // <-- Replace with your Supabase anon key
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
